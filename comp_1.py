79debe2ab79a385db0cae0cf1401c6b85f263bbf
Buse Şenol <44964158+busekoseoglu@users.noreply.github.com>
2026-07-29 15:08:41 +0300

    commmit

--- comp_1.py
+++ comp_1.py
@@ -1,12 +1,38 @@
-def agent_hard_off_talepli_mi(a, ds):
+hard_off_map = {}
+
+for a in AGENTS:
     a = str(a).strip()
-    ds_date = pd.to_datetime(ds).date()
 
-    return ds_date in hard_off_talep_map.get(a, set())
+    hard_days = set()
 
+    hard_days |= izin_map.get(a, set())
+    hard_days |= off_t2_map.get(a, set())
 
-def agent_soft_off_talepli_mi(a, ds):
+    if ENABLE_OFF_T_HARD:
+        hard_days |= off_t_map.get(a, set())
+
+    hard_off_map[a] = hard_days
+
+#-----------------------------
+# x karar değişkeni
+# Hard izin/OFF günlerinde x oluşturulmaz.
+# -----------------------------
+
+x = {}
+
+for a in AGENTS:
     a = str(a).strip()
-    ds_date = pd.to_datetime(ds).date()
 
-    return soft_off_talep_map.get((a, ds_date), 0) == 1
+    for ds in PLAN_GUNLER:
+        ds_date = pd.to_datetime(ds).date()
+
+        # Gerçek izin + off_t2 + hard ise off_t
+        hard_off_gunleri = hard_off_map.get(a, set())
+
+        if ds_date in hard_off_gunleri:
+            continue
+
+        for v in gun_vardiyalari.get(ds, []):
+            x[(a, ds, v)] = model.NewBoolVar(
+                f"x_{a}_{ds}_{v}"
+            )
